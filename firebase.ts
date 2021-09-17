407a1d0fafdf516d5b36fa84c77dfd8f8c43c438
Burhanudin Syed <burhan@Burhanudins-MacBook-Air.local>
2021-09-16 19:19:03 -0500

    removed old dependencies

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,18 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBKf3PecCc71Q1g3xjkPwPj9VaFHdTLdUY",
-  authDomain: "reddit-nextapp.firebaseapp.com",
-  projectId: "reddit-nextapp",
-  storageBucket: "reddit-nextapp.appspot.com",
-  messagingSenderId: "480924373815",
-  appId: "1:480924373815:web:d5570493a47c721740c0b2",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKf3PecCc71Q1g3xjkPwPj9VaFHdTLdUY",
+//   authDomain: "reddit-nextapp.firebaseapp.com",
+//   projectId: "reddit-nextapp",
+//   storageBucket: "reddit-nextapp.appspot.com",
+//   messagingSenderId: "480924373815",
+//   appId: "1:480924373815:web:d5570493a47c721740c0b2",
+// };
 
-const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
+// const app = !firebase.apps.length
+//   ? firebase.initializeApp(firebaseConfig)
+//   : firebase.app();
 
-const db = app.firestore();
+// const db = app.firestore();
 
-export { db };
+// export { db };
