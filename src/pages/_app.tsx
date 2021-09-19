ac724fe7b88555bccd0bcc04264ee8876968dbd4
Burhanudin Syed <burhan@Burhanudins-MacBook-Air.local>
2021-09-19 17:11:25 -0500

    adding google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,41 @@ import "../../styles/globals.css";
 import { Provider } from "next-auth/client";
 import { ThemeProvider } from "next-themes";
 import { MainProvider } from "../MainContext";
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../../lib/gtag'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
+    <>
+    <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     <ThemeProvider attribute="class">
       <MainProvider>
         <Provider session={pageProps.session}>
@@ -12,6 +44,7 @@ function MyApp({ Component, pageProps }) {
         </Provider>
       </MainProvider>
     </ThemeProvider>
+    </>
   );
 }
 
