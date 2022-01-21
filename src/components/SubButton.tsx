0d4d1000b78fbd922341db5dfe842d3c2308458c
Burhan <burhanudinsyed@gmail.com>
2022-01-20 21:23:58 -0600

    update subbutton

--- src/components/SubButton.tsx
+++ src/components/SubButton.tsx
@@ -12,14 +12,13 @@ const SubButton = ({ sub, miniMode = false }) => {
   const subsContext: any = useSubsContext();
   const { mySubs, myLocalSubs, myMultis, subscribe, loadedSubs } = subsContext;
 
-  
   //checking subs
   useEffect(() => {
-    let subbed = false; 
+    let subbed = false;
     if (session) {
       mySubs.forEach((s) => {
         if (s?.data?.name == sub) {
-          subbed = true; 
+          subbed = true;
           setSubbed(true);
           setloadAPI(false);
         }
@@ -31,11 +30,11 @@ const SubButton = ({ sub, miniMode = false }) => {
   }, [session, mySubs, sub]);
   //checking local subs
   useEffect(() => {
-    if (!loading && !session){
-      let subbed = false; 
+    if (!loading && !session) {
+      let subbed = false;
       myLocalSubs.forEach((s) => {
         if (s?.data?.name?.toUpperCase() == sub?.toUpperCase()) {
-          subbed = true; 
+          subbed = true;
           setSubbed(true);
           setloadAPI(false);
         }
@@ -44,9 +43,9 @@ const SubButton = ({ sub, miniMode = false }) => {
       setloadAPI(false);
       //console.log("checked local subs");
     } else if (loading) {
-      //getSession(); causing loop? 
+      //getSession(); causing loop?
     }
-  }, [loading, session, myLocalSubs, sub])
+  }, [loading, session, myLocalSubs, sub]);
 
   const startSubscribe = async (action, sub2sub) => {
     //don't send subscribe request if invalid sub or session not established
@@ -55,61 +54,72 @@ const SubButton = ({ sub, miniMode = false }) => {
       //console.log("attempting", session?.user?.name, action, sub2sub);
       let s = await subscribe(action, sub2sub, session);
       //console.log(s);
-      s && setSubbed(p => !p)
+      s && setSubbed((p) => !p);
       setloadAPI(false);
-     
     }
   };
 
   return (
-      <div className="relative select-none">
-        <div
-          className={
-            (!miniMode ?
-              "w-24 text-center flex justify-center items-center dark:border border-2 dark:border-lightBorder " : " ") +
-            " rounded-md cursor-pointer dark:hover:bg-darkBorder hover:bg-lightHighlight group"
-          }
-        >
-          {subbed && !loadAPI ? (
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                startSubscribe("unsub", sub);
-              }}
-              className="flex items-center p-1 space-x-1 group"
-            >
-              {miniMode ? (
-                <AiOutlineMinus />
-              ) : (
-                <>
-                  <span className="group-hover:hidden">Joined</span>
-                  <span className="hidden group-hover:block">Leave</span>
-                </>
-              )}
-            </div>
-          ) : !subbed && !loadAPI ? (
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                startSubscribe("sub", sub);
-              }}
-              className="flex items-center p-1 space-x-1"
-            >
-              {miniMode ? <AiOutlinePlus /> : <span>Join</span>}
-            </div>
-          ) : loadAPI ? (
+    <div className="relative select-none">
+      <div
+        className={
+          (!miniMode
+            ? "w-24 text-center flex justify-center items-center dark:border border-2 dark:border-lightBorder hover:bg-lightHighlight "
+            : " hover:bg-white") +
+          " rounded-md cursor-pointer dark:hover:bg-darkBorder  group"
+        }
+      >
+        {!loadedSubs ? (
+          <>
             <div className={!miniMode ? "p-2" : ""}>
               <ImSpinner2 className="animate-spin" />
               {/* {subbed ? <span>Unfollow</span> : <span>Follow</span>} */}
             </div>
-          ) : (
-            <div className={!miniMode ? "p-2" : ""}>
-              <ImSpinner2 className="animate-spin" />
-              {/* {subbed ? <span>Unfollow</span> : <span>Follow</span>} */}
-            </div>
-          )}
-        </div>
+          </>
+        ) : (
+          <>
+            {subbed && !loadAPI ? (
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  startSubscribe("unsub", sub);
+                }}
+                className="flex items-center p-1 space-x-1 group"
+              >
+                {miniMode ? (
+                  <AiOutlineMinus />
+                ) : (
+                  <>
+                    <span className="group-hover:hidden">Joined</span>
+                    <span className="hidden group-hover:block">Leave</span>
+                  </>
+                )}
+              </div>
+            ) : !subbed && !loadAPI ? (
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  startSubscribe("sub", sub);
+                }}
+                className="flex items-center p-1 space-x-1"
+              >
+                {miniMode ? <AiOutlinePlus /> : <span>Join</span>}
+              </div>
+            ) : loadAPI ? (
+              <div className={!miniMode ? "p-2" : ""}>
+                <ImSpinner2 className="animate-spin" />
+                {/* {subbed ? <span>Unfollow</span> : <span>Follow</span>} */}
+              </div>
+            ) : (
+              <div className={!miniMode ? "p-2" : ""}>
+                <ImSpinner2 className="animate-spin" />
+                {/* {subbed ? <span>Unfollow</span> : <span>Follow</span>} */}
+              </div>
+            )}
+          </>
+        )}
       </div>
+    </div>
   );
 };
 
