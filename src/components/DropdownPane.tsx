a7749b329bb4706b9d638cd14c373d4a97c708de
Burhan <burhanudinsyed@gmail.com>
2022-02-07 19:14:07 -0600

    dropdown expansion

--- src/components/DropdownPane.tsx
+++ src/components/DropdownPane.tsx
@@ -7,13 +7,18 @@ import { BsChevronDown } from "react-icons/bs";
 import { AiOutlineHome } from "react-icons/ai";
 import { CgLivePhoto } from "react-icons/cg";
 import { BiRightTopArrowCircle } from "react-icons/bi";
-
+import {
+  HiOutlineMinus,
+  HiOutlinePlusSm,
+  HiOutlineMinusSm,
+} from "react-icons/hi";
 // import InfiniteScroll from "react-infinite-scroll-component";
 import Link from "next/link";
 import { signIn, useSession } from "next-auth/client";
 import DropdownItem from "./DropdownItem";
 import DropdownSubCard from "./DropdownSubCard";
 import { useSubsContext } from "../MySubs";
+import { useWindowHeight } from "@react-hook/window-size";
 
 const DropdownPane = ({ hide }) => {
   const subsContext: any = useSubsContext();
@@ -32,7 +37,10 @@ const DropdownPane = ({ hide }) => {
   } = subsContext;
 
   const [show, setShow] = useState(false);
-
+  const [expand, setExpand] = useState(false);
+  const [expandMultis, setExpandMultis] = useState(true);
+  const [expandSubs, setExpandSubs] = useState(true);
+  const windowHeight = useWindowHeight();
   const router = useRouter();
   //const [location, setLocation] = useState("home");
 
@@ -118,12 +126,17 @@ const DropdownPane = ({ hide }) => {
       {/* Dropdown */}
       <div
         className={
-          "flex flex-col w-full transform transition border duration-150 ease-in-out origin-top dark:bg-darkBG bg-white dark:border-darkBorder border-lightBorder mt-1 rounded-md shadow-sm " +
+          " flex flex-col w-full transform transition border duration-150 ease-in-out origin-top dark:bg-darkBG bg-white dark:border-darkBorder border-lightBorder mt-1 rounded-md shadow-sm " +
           `${show && !hide ? " block" : " hidden"}`
         }
       >
         {/* scroll */}
-        <div className="grid grid-cols-1 overflow-y-auto overscroll-contain max-h-96 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full dark:scrollbar-thumb-red-800 ">
+        <div
+          className={
+            "grid grid-cols-1 overflow-y-auto overscroll-contain scrollbar-thin transition-all   scrollbar-thumb-blue-400 scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full dark:scrollbar-thumb-red-800 " +
+            (expand ? " max-h-[90vh]" : "  max-h-96 ")
+          }
+        >
           {/* Quick Links */}
           <div className="flex flex-col py-2 font-light">
             {router.pathname.includes("/r/") &&
@@ -168,46 +181,83 @@ const DropdownPane = ({ hide }) => {
               </a>
             </Link>
           </div>
-
           {!session && (
             <>
               {myLocalMultis?.length > 0 && (
                 <>
-                  <div className="pl-2 text-xs tracking-widest">
-                    local multis
+                  {/* <div className="pl-2 text-xs tracking-widest">
+                      local multis
+                    </div> */}
+                  <div
+                    onClick={() => setExpandMultis((m) => !m)}
+                    className={
+                      "px-2 py-0.5 flex justify-between items-center text-xs tracking-widest hover:font-semibold hover:cursor-pointer hover:bg-lightHighlight dark:hover:bg-darkHighlight" +
+                      (expandMultis ? " " : " mb-2")
+                    }
+                  >
+                    <p>multis</p>
+                    {expandMultis ? <HiOutlineMinusSm /> : <HiOutlinePlusSm />}
                   </div>
-                  <div className="py-2">
-                    {myLocalMultis
-                      ? myLocalMultis.map((multi, i) => {
-                          return (
-                            <div
-                              className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
-                              key={i}
-                            >
-                              <DropdownItem sub={multi} />
-                            </div>
-                          );
-                        })
-                      : ""}
+                  <div
+                    className={
+                      " " +
+                      (expandMultis
+                        ? " max-h-full"
+                        : " max-h-0 overflow-hidden")
+                    }
+                  >
+                    <div className="py-2">
+                      {myLocalMultis
+                        ? myLocalMultis.map((multi, i) => {
+                            return (
+                              <div
+                                className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
+                                key={i}
+                              >
+                                <DropdownItem sub={multi} />
+                              </div>
+                            );
+                          })
+                        : ""}
+                    </div>
                   </div>
                 </>
               )}
               {myLocalSubs?.length > 0 ? (
                 <>
-                  <div className="pl-2 text-xs tracking-widest">local subs</div>
-                  <div className="py-2">
-                    {myLocalSubs
-                      ? myLocalSubs.map((sub, i) => {
-                          return (
-                            <div
-                              className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
-                              key={i}
-                            >
-                              <DropdownItem sub={sub} />
-                            </div>
-                          );
-                        })
-                      : ""}
+                  {/* <div className="pl-2 text-xs tracking-widest">
+                      local subs
+                    </div> */}
+                  <div
+                    onClick={() => setExpandSubs((m) => !m)}
+                    className={
+                      "px-2 py-0.5 flex justify-between items-center text-xs tracking-widest hover:font-semibold hover:cursor-pointer hover:bg-lightHighlight dark:hover:bg-darkHighlight" +
+                      (expandMultis ? " " : " mb-2")
+                    }
+                  >
+                    <p>local subs</p>
+                    {expandMultis ? <HiOutlineMinusSm /> : <HiOutlinePlusSm />}
+                  </div>
+                  <div
+                    className={
+                      " " +
+                      (expandSubs ? " max-h-full" : " max-h-0 overflow-hidden")
+                    }
+                  >
+                    <div className="py-2">
+                      {myLocalSubs
+                        ? myLocalSubs.map((sub, i) => {
+                            return (
+                              <div
+                                className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
+                                key={i}
+                              >
+                                <DropdownItem sub={sub} />
+                              </div>
+                            );
+                          })
+                        : ""}
+                    </div>
                   </div>
                 </>
               ) : (
@@ -228,88 +278,120 @@ const DropdownPane = ({ hide }) => {
             <>
               {/* Multis */}
               {/* onClick={() => {setloadedMultis(m => !m);setloadedSubs(s => !s)}} */}
-              <div className="pl-2 text-xs tracking-widest">multis</div>
-              {!loadedMultis ? (
-                // Loading pane
-                <>
-                  <div className="py-2">
-                    <div className="px-4 py-1 ">
-                      {/* Repeated rows */}
-                      {[...Array(3)].map((u, i) => (
-                        <div key={i} className="py-1">
-                          <div className="flex flex-row items-center text-sm text-center animate-pulse ">
-                            {/* Image */}
-                            <div className="flex flex-row items-center w-6 h-6 ml-1 ">
-                              <div className="w-6 h-6 text-center bg-red-400 rounded text-lightText ">
-                                {"m"}
+              <div
+                onClick={() => setExpandMultis((m) => !m)}
+                className={
+                  "px-2 py-0.5 flex justify-between items-center text-xs tracking-widest hover:font-semibold hover:cursor-pointer hover:bg-lightHighlight dark:hover:bg-darkHighlight" +
+                  (expandMultis ? " " : " mb-2")
+                }
+              >
+                <p>multis</p>
+                {expandMultis ? <HiOutlineMinusSm /> : <HiOutlinePlusSm />}
+              </div>
+              <div
+                className={
+                  " " +
+                  (expandMultis ? " max-h-full" : " max-h-0 overflow-hidden")
+                }
+              >
+                {!loadedMultis ? (
+                  // Loading pane
+                  <>
+                    <div className="py-2">
+                      <div className="px-4 py-1 ">
+                        {/* Repeated rows */}
+                        {[...Array(3)].map((u, i) => (
+                          <div key={i} className="py-1">
+                            <div className="flex flex-row items-center text-sm text-center animate-pulse ">
+                              {/* Image */}
+                              <div className="flex flex-row items-center w-6 h-6 ml-1 ">
+                                <div className="w-6 h-6 text-center bg-red-400 rounded text-lightText ">
+                                  {"m"}
+                                </div>
                               </div>
+                              {/* Text */}
+                              <div className="w-full h-6 ml-2 bg-gray-300 rounded dark:bg-gray-800 "></div>
                             </div>
-                            {/* Text */}
-                            <div className="w-full h-6 ml-2 bg-gray-300 rounded dark:bg-gray-800 "></div>
                           </div>
-                        </div>
-                      ))}
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="py-2">
-                    {myMultis
-                      ? myMultis.map((multi, i) => {
+                  </>
+                ) : (
+                  <>
+                    <div className="py-2">
+                      {myMultis
+                        ? myMultis.map((multi, i) => {
+                            return (
+                              <div
+                                className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
+                                key={`${i}_${multi.data.display_name}`}
+                              >
+                                <DropdownItem sub={multi} />
+                              </div>
+                            );
+                          })
+                        : ""}
+                    </div>
+                  </>
+                )}
+              </div>
+              {/* Subs */}
+              <div
+                onClick={() => setExpandSubs((e) => !e)}
+                className={
+                  "px-2 py-0.5 items-center text-xs tracking-widest hover:bg-lightHighlight dark:hover:bg-darkHighlight  hover:cursor-pointer hover:font-semibold flex flex-row justify-between" +
+                  (expandSubs ? " " : " mb-2")
+                }
+              >
+                <p>subs</p>
+                {expandSubs ? <HiOutlineMinusSm /> : <HiOutlinePlusSm />}
+              </div>
+              <div
+                className={
+                  " " +
+                  (expandSubs ? " max-h-full" : " max-h-0 overflow-hidden")
+                }
+              >
+                {!loadedSubs ? (
+                  <>
+                    <div className="py-2">
+                      <div className="px-4 py-1 ">
+                        {/* Repeated rows */}
+                        {[...Array(5)].map((u, i) => (
+                          <div key={i} className="py-1">
+                            <div className="flex flex-row items-center text-sm text-center animate-pulse ">
+                              {/* Image */}
+                              <div className="flex flex-row items-center w-6 h-6 ml-1 ">
+                                <div className="w-6 h-6 text-center bg-blue-700 rounded-full text-lightText ">
+                                  {"r/"}
+                                </div>
+                              </div>
+                              {/* Text */}
+                              <div className="w-full h-6 ml-2 bg-gray-300 rounded dark:bg-gray-800 "></div>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div className={"py-2"}>
+                    {mySubs
+                      ? mySubs.map((sub, i) => {
                           return (
                             <div
                               className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
-                              key={`${i}_${multi.data.display_name}`}
+                              key={i}
                             >
-                              <DropdownItem sub={multi} />
+                              <DropdownItem sub={sub} />
                             </div>
                           );
                         })
                       : ""}
                   </div>
-                </>
-              )}
-              {/* Subs */}
-              <div className="pl-2 text-xs tracking-widest">subs</div>
-              {!loadedSubs ? (
-                <>
-                  <div className="py-2">
-                    <div className="px-4 py-1 ">
-                      {/* Repeated rows */}
-                      {[...Array(5)].map((u, i) => (
-                        <div key={i} className="py-1">
-                          <div className="flex flex-row items-center text-sm text-center animate-pulse ">
-                            {/* Image */}
-                            <div className="flex flex-row items-center w-6 h-6 ml-1 ">
-                              <div className="w-6 h-6 text-center bg-blue-700 rounded-full text-lightText ">
-                                {"r/"}
-                              </div>
-                            </div>
-                            {/* Text */}
-                            <div className="w-full h-6 ml-2 bg-gray-300 rounded dark:bg-gray-800 "></div>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </>
-              ) : (
-                <div className="py-2">
-                  {mySubs
-                    ? mySubs.map((sub, i) => {
-                        return (
-                          <div
-                            className="px-4 py-2 hover:bg-lightHighlight dark:hover:bg-darkHighlight"
-                            key={i}
-                          >
-                            <DropdownItem sub={sub} />
-                          </div>
-                        );
-                      })
-                    : ""}
-                </div>
-              )}
+                )}
+              </div>
             </>
           )}
           {session && error && (
@@ -320,6 +402,14 @@ const DropdownPane = ({ hide }) => {
             </>
           )}
         </div>
+        {true && (
+          <div
+            onClick={() => setExpand((e) => !e)}
+            className="flex items-center justify-center border-t border-lightBorder dark:border-darkBorder hover:cursor-pointer hover:bg-lightHighlight dark:hover:bg-darkPostHover"
+          >
+            <HiOutlineMinus className="w-6 h-3 " />
+          </div>
+        )}
       </div>
     </div>
   );
