69b6dbd5e53f260b87128c561adad12554f385ec
Burhan Syed <burhanudinsyed@gmail.com>
2022-07-28 17:49:44 -0400

    shortlink direct load

--- src/pages/[frontsort]/index.tsx
+++ src/pages/[frontsort]/index.tsx
@@ -5,22 +5,50 @@ import NavBar from "../../components/NavBar";
 import Head from "next/head";
 import PostModal from "../../components/PostModal";
 import LoginModal from "../../components/LoginModal";
+import { loadPost } from "../../RedditAPI";
+import { findMediaInfo } from "../../../lib/utils";
 
-const Subs = ({ query }) => {
+const Subs = ({ query, metaTags, post }) => {
   return (
     <div>
       <Head>
         <title>
-          {query?.frontsort ? `troddit · ${query?.frontsort}` : "troddit"}
+          {metaTags?.ogTitle ? `troddit · ${metaTags?.ogTitle}` : query?.frontsort ? `troddit · ${query?.frontsort}` : "troddit"}
         </title>
+        {metaTags?.ogSiteName && (
+          <>
+            <meta property="og:site_name" content={metaTags?.ogSiteName} />
+            {metaTags?.ogDescription && (
+              <meta
+                property="og:description"
+                content={metaTags?.ogDescription}
+              />
+            )}
+            {metaTags?.ogTitle && (
+              <meta property="og:title" content={metaTags?.ogTitle} />
+            )}
+            {metaTags?.ogImage && (
+              <meta property="og:image" content={metaTags?.ogImage} />
+            )}
+            {metaTags?.ogHeight && (
+              <meta property="og:image:height" content={metaTags?.ogHeight} />
+            )}
+            {metaTags?.ogWidth && (
+              <meta property="og:image:width" content={metaTags?.ogWidth} />
+            )}
+            {metaTags?.ogType && (
+              <meta property="og:type" content={metaTags?.ogType} />
+            )}
+          </>
+        )}
       </Head>
 
       <main className="">
-        {(query.frontsort === "best" ||
+        {query.frontsort === "best" ||
         query.frontsort === "hot" ||
         query.frontsort === "new" ||
         query.frontsort === "top" ||
-        query.frontsort === "rising") ? (
+        query.frontsort === "rising" ? (
           <>
             <Feed />
           </>
@@ -33,8 +61,9 @@ const Subs = ({ query }) => {
                 returnRoute={query?.slug?.[0] ? `/r/${query?.slug[0]}` : "/"}
                 setSelect={() => {}}
                 direct={true}
-                curKey={""}
-                postNum={0}
+                curKey={undefined}
+                postNum={undefined}
+                postData={post}
               />
             </div>
           </>
@@ -44,7 +73,47 @@ const Subs = ({ query }) => {
   );
 };
 
-Subs.getInitialProps = ({ query }) => {
+Subs.getInitialProps = async (d) => {
+  const { query, req } = d;
+  const url = req?.url;
+  if (
+    !(
+      query.frontsort === "best" ||
+      query.frontsort === "hot" ||
+      query.frontsort === "new" ||
+      query.frontsort === "top" ||
+      query.frontsort === "rising"
+    )
+  ) {
+    try {
+      console.log(url);
+      const { post } = await loadPost(url);
+      //const data = await fetch(`https://www.reddit.com${url}.json`)
+      //let post = (await data.json())?.[0]?.data?.children?.[0]?.data;
+      const media = await findMediaInfo(
+        post,
+        true,
+        d?.req?.headers.host?.split(":")?.[0]
+      );
+      console.log(post, media);
+      let metaTags = {
+        ogSiteName: "troddit",
+        ogDescription: `Post on r/${post.subreddit} by u/${
+          post.author
+        } • ${post.score?.toLocaleString(
+          "en-US"
+        )} points and ${post.num_comments?.toLocaleString("en-US")} comments`,
+        ogTitle: post.title,
+        ogImage: media?.imageInfo?.[media?.imageInfo?.length - 1]?.url,
+        ogHeight: media?.dimensions?.[1],
+        ogWidth: media?.dimensions?.[0],
+        ogType: `image`,
+      };
+      return { query, metaTags, post: post?.preview ? post : undefined };
+    } catch (err) {
+      return { query };
+    }
+  }
   return { query };
 };
 
