37da24f1ce3b664b842f57b650c7da572b4315e2
Burhan Syed <burhanudinsyed@gmail.com>
2023-04-25 15:38:32 -0400

    seperate post body from media

--- src/components/ParseBodyHTML.tsx
+++ src/components/ParseBodyHTML.tsx
@@ -2,58 +2,7 @@
 import { useTheme } from "next-themes";
 import React, { useEffect, useRef, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import ParseATag from "./ParseATag";
-
-import HtmlToReact from "html-to-react";
-
-const HtmlToReactParser = HtmlToReact.Parser;
-const htmlToReactParser = new HtmlToReactParser();
-const isValidNode = function () {
-  return true;
-};
-
-// Order matters. Instructions are processed in the order they're defined
-const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
-const processingInstructions = [
-  {
-    shouldProcessNode: function (node) {
-      let check =
-        node.parent &&
-        node.parent.name &&
-        node.parent.name === "a" &&
-        node.parent?.attribs?.href?.includes("https://") &&
-        checkSupport(node.parent?.attribs?.href, node) &&
-        node.name !== "img"; //leave comment gifs alone
-      return check;
-    },
-    processNode: function (node, children, index) {
-      return React.createElement(ParseATag, { key: index }, node); //node?.data?.toUpperCase();
-    },
-  },
-  {
-    // Anything else
-    shouldProcessNode: function (node) {
-      return true;
-    },
-    processNode: processNodeDefinitions.processDefaultNode,
-  },
-];
-const checkSupport = (link: string, node: any) => {
-  //prevent recurring nodes from all having expansion buttons
-  if (node?.next?.parent?.attribs?.href === link) {
-    return false;
-  }
-
-  let imgurRegex = /([A-z.]+\.)?(imgur(\.com))+(\/)+([A-z0-9]){7}\./gm;
-  let redditRegex =
-    /(preview+\.)+(reddit(\.com)|redd(\.it))+(\/[A-z0-9]+)+(\.(png|jpg))\./gm;
-  let greedyRegex = /(\.(png|jpg))/gm;
-  return !!(
-    link.match(imgurRegex) ||
-    link.match(redditRegex) ||
-    link.match(greedyRegex)
-  );
-};
+import useParseBodyHTML from "../hooks/useParseBodyHTML";
 
 const ErrorFallBack = () => {
   return (
@@ -73,97 +22,14 @@ const ParseBodyHTML = ({
   comment = false,
   newTabLinks = true,
 }) => {
+  const component = useParseBodyHTML({ rawHTML: html, newTabLinks });
   const { theme, resolvedTheme } = useTheme();
-  const [component, setComponent] = useState<any>();
   const ref = useRef<any>();
 
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
   }, []);
-
-  useEffect(() => {
-    //formatting per Teddit
-    // const unescape = (s) => {
-    //   if (s) {
-    //     var re = /&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34);/g;
-    //     var unescaped = {
-    //       "&amp;": "&",
-    //       "&#38;": "&",
-    //       "&lt;": "<",
-    //       "&#60;": "<",
-    //       "&gt;": ">",
-    //       "&#62;": ">",
-    //       "&apos;": "'",
-    //       "&#39;": "'",
-    //       "&quot;": '"',
-    //       "&#34;": '"',
-    //     };
-    //     let result = s.replace(re, (m) => {
-    //       return unescaped[m];
-    //     });
-    //     if (newTabLinks) {
-    //       result = blankTargets(result);
-    //     }
-    //     result = replaceDomains(result);
-    //     return result;
-    //   } else {
-    //     return "";
-    //   }
-    // };
-    const DOMAIN = window?.location?.host ?? "troddit.com";
-
-    const blankTargets = (str) => {
-      if (str?.includes("<a ")) {
-        str = str?.replaceAll("<a ", '<a target="_blank" rel="noreferrer" ');
-      }
-      return str;
-    };
-
-    const replaceDomains = (str) => {
-      if (typeof str == "undefined" || !str) return;
-      let splitstr = str.split("<a");
-      let replaceall: string[] = [];
-      splitstr.forEach((substr) => replaceall.push(replaceUserDomains(substr)));
-      return replaceall.join("<a");
-    };
-
-    const replaceUserDomains = (str: string) => {
-      let redditRegex = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))/gm;
-      let matchRegex1 = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/[ru]\/)/gm;
-      let matchRegex2 = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/user\/)/gm;
-      let matchRegex3 =
-        /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/)+([A-z0-9]){6}("|\s)/gm;
-      // let youtubeRegex = /([A-z.]+\.)?youtu(be\.com|\.be)/gm;
-      // let twitterRegex = /([A-z.]+\.)?twitter\.com/gm;
-      // let instagramRegex = /([A-z.]+\.)?instagram.com/gm;
-      if (
-        str.match(matchRegex1) ||
-        str.match(matchRegex2) ||
-        str.match(matchRegex3)
-      ) {
-        str = str.replace(redditRegex, DOMAIN);
-      }
-      return str;
-    };
-
-    const parseHTML = (html) => {
-      const reactElement = htmlToReactParser.parseWithInstructions(
-        html,
-        isValidNode,
-        processingInstructions
-      );
-      return reactElement;
-    };
-
-    //let unescaped = unescape(html); //no longer need this due to html-to-react
-    let result = replaceDomains(html);
-    if (newTabLinks) {
-      result = blankTargets(result);
-    }
-    let reactElement = parseHTML(result);
-    setComponent(reactElement);
-  }, [html]);
 
   //allow single click to open links, posts..
   useEffect(() => {
---
+++ src/hooks/useParseBodyHTML.tsx
@@ -0,0 +1,118 @@
+/* eslint-disable react/display-name */
+import React, { useEffect, useState } from "react";
+import ParseATag from "../components/ParseATag";
+
+import HtmlToReact from "html-to-react";
+
+const HtmlToReactParser = HtmlToReact.Parser;
+const htmlToReactParser = new HtmlToReactParser();
+const isValidNode = function () {
+  return true;
+};
+
+// Order matters. Instructions are processed in the order they're defined
+const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
+const processingInstructions = [
+  {
+    shouldProcessNode: function (node) {
+      let check =
+        node.parent &&
+        node.parent.name &&
+        node.parent.name === "a" &&
+        node.parent?.attribs?.href?.includes("https://") &&
+        checkSupport(node.parent?.attribs?.href, node) &&
+        node.name !== "img"; //leave comment gifs alone
+      return check;
+    },
+    processNode: function (node, children, index) {
+      return React.createElement(ParseATag, { key: index }, node); //node?.data?.toUpperCase();
+    },
+  },
+  {
+    // Anything else
+    shouldProcessNode: function (node) {
+      return true;
+    },
+    processNode: processNodeDefinitions.processDefaultNode,
+  },
+];
+const checkSupport = (link: string, node: any) => {
+  //prevent recurring nodes from all having expansion buttons
+  if (node?.next?.parent?.attribs?.href === link) {
+    return false;
+  }
+
+  let imgurRegex = /([A-z.]+\.)?(imgur(\.com))+(\/)+([A-z0-9]){7}\./gm;
+  let redditRegex =
+    /(preview+\.)+(reddit(\.com)|redd(\.it))+(\/[A-z0-9]+)+(\.(png|jpg))\./gm;
+  let greedyRegex = /(\.(png|jpg))/gm;
+  return !!(
+    link.match(imgurRegex) ||
+    link.match(redditRegex) ||
+    link.match(greedyRegex)
+  );
+};
+
+const useParseBodyHTML = ({ rawHTML, newTabLinks = false }) => {
+  const [component, setComponent] = useState<any>();
+
+  useEffect(() => {
+    const DOMAIN = window?.location?.host ?? "troddit.com";
+
+    const blankTargets = (str) => {
+      if (str?.includes("<a ")) {
+        str = str?.replaceAll("<a ", '<a target="_blank" rel="noreferrer" ');
+      }
+      return str;
+    };
+
+    const replaceDomains = (str) => {
+      if (typeof str == "undefined" || !str) return;
+      let splitstr = str.split("<a");
+      let replaceall: string[] = [];
+      splitstr.forEach((substr) => replaceall.push(replaceUserDomains(substr)));
+      return replaceall.join("<a");
+    };
+
+    const replaceUserDomains = (str: string) => {
+      let redditRegex = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))/gm;
+      let matchRegex1 = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/[ru]\/)/gm;
+      let matchRegex2 = /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/user\/)/gm;
+      let matchRegex3 =
+        /([A-z.]+\.)?(reddit(\.com)|redd(\.it))+(\/)+([A-z0-9]){6}("|\s)/gm;
+      // let youtubeRegex = /([A-z.]+\.)?youtu(be\.com|\.be)/gm;
+      // let twitterRegex = /([A-z.]+\.)?twitter\.com/gm;
+      // let instagramRegex = /([A-z.]+\.)?instagram.com/gm;
+      if (
+        str.match(matchRegex1) ||
+        str.match(matchRegex2) ||
+        str.match(matchRegex3)
+      ) {
+        str = str.replace(redditRegex, DOMAIN);
+      }
+      return str;
+    };
+
+    const parseHTML = (html) => {
+      const reactElement = htmlToReactParser.parseWithInstructions(
+        html,
+        isValidNode,
+        processingInstructions
+      );
+      return reactElement;
+    };
+
+    //let unescaped = unescape(html); //no longer need this due to html-to-react
+    let result = replaceDomains(rawHTML);
+    if (newTabLinks) {
+      result = blankTargets(result);
+    }
+    let reactElement = parseHTML(result);
+    setComponent(reactElement);
+  }, [rawHTML]);
+
+  return component; 
+
+};
+
+export default useParseBodyHTML;
