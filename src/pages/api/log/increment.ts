217fa71402b22272d195b9568953df475782c49d
Burhan Syed <burhanudinsyed@gmail.com>
2023-06-07 15:02:44 -0500

    opt in api logging

--- src/pages/api/log/increment.ts
+++ src/pages/api/log/increment.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import supabase from "../../../lib/supabase";
 import { ROUTES_TYPES } from "../../../../types/logs";
-
+import { createClient } from "@supabase/supabase-js";
+const ENABLE_API_LOG = JSON.parse(process?.env?.ENABLE_API_LOG ?? "false");
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 export const config = {
   runtime: "experimental-edge",
 };
@@ -19,20 +21,30 @@ const handler = async (request: NextRequest) => {
       statusText: "invalid body",
     });
   }
-  let { data, error } = await supabase.rpc("increment_log", {
-    c_date: new Date(),
-    is_oauth: body?.is_oauth ?? false,
-    route_type: body.route_type,
-  });
-
-  if (error) {
-    console.error("Err?", error);
-    return new NextResponse(undefined, {
-      status: 500,
-      statusText: "Log Error",
+  if (ENABLE_API_LOG) {
+    if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
+      throw new Error("Missing Supabase Credentials");
+    }
+    const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+    let { data, error } = await supabase.rpc("increment_log", {
+      c_date: new Date(),
+      is_oauth: body?.is_oauth ?? false,
+      route_type: route_type,
     });
+
+    if (error) {
+      console.error("Err?", error);
+      return new NextResponse(undefined, {
+        status: 500,
+        statusText: "Log Error",
+      });
+    }
   }
-  return new NextResponse(undefined, { status: 200 });
+
+  return new NextResponse(undefined, {
+    status: 200,
+    statusText: ENABLE_API_LOG ? `logged ${route_type}` : "logging disabled",
+  });
 };
 
 export default handler;
