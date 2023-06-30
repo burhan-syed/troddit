fec63af068933994caa6a445f0da376a9aee174c
Burhan Syed <burhanudinsyed@gmail.com>
2023-06-29 22:46:14 -0400

    basic email validation

--- src/pages/api/form/email.ts
+++ src/pages/api/form/email.ts
@@ -6,12 +6,21 @@ const ACCEPT_EMAILS = JSON.parse(
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
+const validateEmail = (email: string) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+};
+
 export default async function handler(req, res) {
   const body = req.body;
 
   if (!body.email) {
     // Sends a HTTP bad request error code
     return res.status(400).json({ data: "email not found" });
+  }
+
+  if (!validateEmail(body.email)) {
+    return res.status(400).json({ data: "invalid email" });
   }
 
   if (ACCEPT_EMAILS) {
@@ -25,12 +34,15 @@ export default async function handler(req, res) {
       .insert([{ email: body.email }])
       .select();
 
-
-    if (error && (error.code !== "23505")) {
+    if (error && error.code !== "23505") {
       console.error("Err?", error);
       return new res.status(500).json({ data: "something went wrong" });
     }
-    return res.status(200).json({ data: `email submitted, you will be contacted when sign ups are available` });
+    return res
+      .status(200)
+      .json({
+        data: `email submitted, you will be contacted when sign ups are available`,
+      });
   }
   return res.status(401).json({ data: "not accepting emails" });
 }
